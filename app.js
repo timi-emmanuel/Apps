9d18295692b2dbcb125593689f92517a2f94dd08
timi-emmanuel <121458382+timi-emmanuel@users.noreply.github.com>
2025-01-20 16:54:10 +0100

    Your commit message here

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// get elements
-const access = document.querySelector('#access-denied');
+document.addEventListener('DOMContentLoaded',() => {
+// Weather App
+if (document.body.classList.contains('bg-[#293251]')){
+  const access = document.querySelector('#access-denied');
 const accessMessage = document.querySelector('#access-denied > p')
 const weatherIcon =document.querySelector('#weather-icon');
 const tempValue = document.querySelector('#temp-value');
 const tempDescription = document.querySelector('#temp-description');
 const Location = document.querySelector('#location');
-const humidity = document.querySelector('#humidity')
-
+const humidity = document.querySelector('#humidity');
 const weather = {};
 weather.temp = {
  unit: 'celsius'};
@@ -142,9 +143,122 @@ document.getElementById('getWeatherByDetails').addEventListener('click', () => {
     output.textContent = 'Please enter a city name or coordinates!';
   }
  });
+}
 
- 
- 
+// Language Translation App 
+  if(document.body.classList.contains('bg-[#5372F0]')){
+    const selectTag = document.querySelectorAll('select'),
+    fromText = document.querySelector('#from-text'),
+    toText = document.querySelector('#to-text'),
+    translateBtn = document.querySelector('button'),
+    exchangeIcon  = document.getElementById('exchange'),
+    icons = document.querySelectorAll('#icons i'); 
+
+    selectTag.forEach( (tag,index) => {
+      for (const country_code in countries){
+        // setting English as From and french as To default language 
+        let selected = '';
+        if(index == 0 && country_code == 'en-GB'){
+           selected = 'selected';
+        }else if(index == 1 && country_code == 'fr-FR'){
+           selected = 'selected';
+        }
+        let option =`<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend",option)
+      }})
+    
+
+    fromText.addEventListener('input', () => {
+      toText.value= '';
+      toText.setAttribute('placeholder', 'translation')
+    })
+    translateBtn.addEventListener('click', () => {
+      let text = fromText.value,
+      translateFrom = selectTag[0].value,
+      translateTo = selectTag[1].value;
+      if (!text){
+        return;
+      }  
+      toText.value = '';  // Clear the previous translation
+      toText.setAttribute('placeholder', 'translating.....')
+      
+      let apiKey= `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+      async function getTranslation (){
+        try{
+          let response = await fetch(apiKey);
+          console.log(response)
+          if (response.status !== 200 || !response.ok){
+          throw new Error('could not fetch data as word might not exist')       
+          }
+    
+          let data = await response.json();
+          toText.value = data.responseData.translatedText;
+          toText.setAttribute('placeholder', 'Translation')
+
+        }catch(error){
+          toText.setAttribute('placeholder', `${error.message}`)
+          console.log(`Error: ${error.message}`);
+        }     
+      }
+      getTranslation()
+    })
+    
+  exchangeIcon.addEventListener('click', () => {
+      let tempText = fromText.value;
+      tempLang = selectTag[0].value;
+      fromText.value = toText.value;
+      selectTag[0].value = selectTag[1].value;
+      toText.value = tempText;
+      selectTag[1].value=tempLang  
+    })
+    
+    icons.forEach( icon =>{
+      icon.addEventListener( 'click', ({target}) => {
+        if(target.classList.contains('fa-copy')){
+          if(target.id == 'from'){
+            navigator.clipboard.writeText(fromText.value)
+          }else{
+            navigator.clipboard.writeText(toText.value)
+          }
+        } else {
+          if(target.id == 'from'){
+            speakTranslatedText(fromText.value, selectTag[0].value )
+          }else{
+            speakTranslatedText(toText.value, selectTag[1].value )
+          }
+        }
+      })
+    })
+
+function speakTranslatedText(text, lang) {
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = lang;  
+      // Find a voice matching the language
+      const voices = speechSynthesis.getVoices();
+      const matchingVoice = voices.find(voice => voice.lang === lang);  
+      if (matchingVoice) {
+        utterance.voice = matchingVoice;
+      } else {
+        console.warn(`No voice found for language: ${lang}`);
+      }  
+      speechSynthesis.speak(utterance);
+    }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
       fontFamily:{
         poppins:['Poppins'],
         roboto:['Roboto']
+      },
+      animation: {
+        fadeIn : 'fadeIn 2s ease-in-out',
+        typing: 'typing 4s steps(30, end), blink 0.5s step-end infinite',
+      },
+      keyframes: {
+        fadeIn : {
+          '0%': {opacity : 0},
+          '100%': {opacity : 1},
+        },
+        typing: {
+          '0%': { width: '0' },
+          '100%': { width: '100%' },
+        },
+        blink: {
+          '50%': { 'border-color': 'transparent' },
+        },
       }
     },
   },
